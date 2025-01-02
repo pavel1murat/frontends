5e4b020b09a93de79a4933daa3f3032a32fa5fd2
murat <mu2etrk@mu2edaq11.fnal.gov>
2025-01-02 09:19:57 -0600

    - updates

--- scripts/start_node_frontend.sh
+++ scripts/start_node_frontend.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/bash
 
 remote_node=$1
+    verbose=0; if [ ".$2" != "." ] ; then verbose=$2 ; fi
 #------------------------------------------------------------------------------
-# start trk DTC control on a remote node
+# start dtc_frontend on a remote node
 # the node names may depend on the subnet
 #------------------------------------------------------------------------------
 local_node=`hostname -s`
 
-frontend=trk_dtc_ctl_frontend
+frontend=dtc_mt_frontend
+midas_host=`hostname -s`-ctrl.fnal.gov
+
+echo LINENO=$LINENO verbose=$verbose remote_mode=$remote_node
 
 if   [ $remote_node == $local_node ] ; then
     $frontend &
-elif [ $remote_node == "mu2edaq22" ] ; then
+else
 #------------------------------------------------------------------------------
 # 1. mu2edaq22 --> mu2edaq22-ctrl (assume Edwards Center)
 #------------------------------------------------------------------------------
     cmd="export MU2E_DAQ_DIR=$MU2E_DAQ_DIR"
     cmd=$cmd"; cd $MU2E_DAQ_DIR"
     cmd=$cmd"; source setup_daq.sh"
-    cmd=$cmd"; $frontend -h `hostname -s`-ctrl.fnal.gov"
-    cmd=$cmd"&"
-    ssh mu2etrk@$remote_node.fnal.gov  $cmd
+    cmd=$cmd"; $frontend -h $midas_host"
+#    cmd=$cmd"&"
+    if [ $verbose != 0 ] ; then echo $cmd ; fi
+    ssh -KX mu2etrk@$remote_node.fnal.gov  $cmd
 fi
